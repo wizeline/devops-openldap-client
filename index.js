7c5245dfee0da9d4da936178ace0575c0280523e
Cristian D. Velazquez Ramirez <cristian.vlz@wizeline.com>
2017-03-02 12:45:29 -0600

    Find in which port the callback server needs to bind

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config({ path: __dirname + '/.env' });
 const opn = require('opn');
 const request = require('request');
 const crypto = require('crypto');
+const async = require('async');
 const Q = require('q');
 const http = require('http');
 const co = require('co');
@@ -13,9 +14,61 @@ const os = require('os');
 const prompt = require('co-prompt');
 
 const defaultPem = 'id_rsa.pub';
-const PORT = process.env.APP_PORT || 3000;
+const START_PORT_RANGE = process.env.START_PORT_RANGE || 9000;
+const PORTS_SIZE = process.env.PORTS_SIZE || 21;
+const PORT_RANGES = (new Array(PORTS_SIZE).fill(0)).map(function(value, number) { return START_PORT_RANGE + number });
 const LDAP_SERVER = process.env.LDAP_SERVER || 'http://httpbin.org/post';
 const promise = Q.defer();
+var PORT;
+
+function openBrowser(env) {
+  var authorize_url = env.AUTH0_URL + '/authorize?response_type=code&scope=openid%20profile&' +
+    'client_id=' + env.AUTH0_CLIENT_ID + '&redirect_uri=' + env.AUTH0_CALLBACK_URL +
+    '&code_challenge=' + env.VERIFIER_CHALLENGUE + '&code_challenge_method=S256';
+  opn(authorize_url)
+    .catch(function(err) {
+      console.log('Error opening ' + authorize_url);
+    });
+}
+
+function getAuth0Token(env) {
+  promise.promise
+    .then(function(code) {
+      request.post(env.AUTH0_URL + '/oauth/token',
+        {
+          json: {
+            code: code,
+            code_verifier: env.VERIFIER,
+            client_id: env.AUTH0_CLIENT_ID,
+            grant_type: 'authorization_code',
+            redirect_uri: env.AUTH0_CALLBACK_URL
+          }
+        },
+        function(err, response, body){
+          if (err) {
+            console.log('ERROR: ' + err);
+            process.exit(-1);
+          }
+
+          if (!response) {
+            console.log('ERROR: Response not found');
+            process.exit(-1);
+          }
+
+          if (response.statusCode !== 200) {
+            console.log('ERROR: Response code is ' + response.statusCode);
+            process.exit(-1);
+          }
+
+          sendPem(body.id_token);
+        }
+      );
+    })
+    .fail(function(err) {
+      console.log(err);
+      process.exit(-1);
+    });
+}
 
 function base64url(b) {
   return b.toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
@@ -28,6 +81,7 @@ function sendPem(idToken) {
   }
 
   co(function *() {
+    // Asks for the filename of the key to send
     const defaultPemFlag = yield prompt('Which pem do you want to use[' + defaultPem + ']?');
     const pubFile = os.homedir() + '/.ssh/' + (defaultPemFlag || defaultPem);
     try {
@@ -55,68 +109,37 @@ function sendPem(idToken) {
   });
 }
 
-http.createServer(function (req, res) {
-  var matches = req.url.match(/code=([^&]+).*$/);
-  if (matches instanceof Array && matches.length >= 2) {
-    promise.resolve(matches[1]);
-    res.write('Success, close the tab and go back to the terminal');
-  } else {
-    promise.reject('Unable to find code');
-  }
-  res.end();
-}).listen(PORT);
+// Tries to bind the server to an available port
+var index = 0;
+async.retry(PORTS_SIZE, function(callback, results) {
+  const port = PORT_RANGES[index];
+  index++;
 
-var env = {
-  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
-  AUTH0_URL: process.env.AUTH0_URL,
-  AUTH0_CALLBACK_URL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:' + PORT + '/callback',
-};
+  var server = http.createServer(function (req, res) {
+    var matches = req.url.match(/code=([^&]+).*$/);
+    if (matches instanceof Array && matches.length >= 2) {
+      promise.resolve(matches[1]);
+      res.write('Success, close the tab and go back to the terminal');
+    } else {
+      promise.reject('Unable to find code');
+    }
+    res.end();
+  }).listen(port, '0.0.0.0');
 
-//Generate the verifier, and the corresponding challenge
-var verifier = base64url(crypto.randomBytes(32));
-var verifier_challenge = base64url(crypto.createHash('sha256').update(verifier).digest());
+  server.on('error', callback);
 
-var authorize_url = env.AUTH0_URL + '/authorize?response_type=code&scope=openid%20profile&' +
-  'client_id=' + env.AUTH0_CLIENT_ID + '&redirect_uri=' + env.AUTH0_CALLBACK_URL +
-  '&code_challenge=' + verifier_challenge + '&code_challenge_method=S256';
-opn(authorize_url)
-  .catch(function(err) {
-    console.log('Error opening ' + authorize_url);
-  });
+  server.on('listening', callback.bind(null, null, port));
+}, function(err, port) {
+  //Generate the verifier, and the corresponding challenge
+  const verifier = base64url(crypto.randomBytes(32));
+  var env = {
+    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    AUTH0_URL: process.env.AUTH0_URL,
+    AUTH0_CALLBACK_URL: 'http://localhost:' + port + '/callback',
+    VERIFIER: verifier,
+    VERIFIER_CHALLENGUE: base64url(crypto.createHash('sha256').update(verifier).digest())
+  };
 
-promise.promise
-  .then(function(code) {
-    request.post(env.AUTH0_URL + '/oauth/token',
-      {
-        json: {
-          code: code,
-          code_verifier: verifier,
-          client_id: env.AUTH0_CLIENT_ID,
-          grant_type: 'authorization_code',
-          redirect_uri: env.AUTH0_CALLBACK_URL
-        }
-      },
-      function(err, response, body){
-        if (err) {
-          console.log('ERROR: ' + err);
-          process.exit(-1);
-        }
-
-        if (!response) {
-          console.log('ERROR: Response not found');
-          process.exit(-1);
-        }
-
-        if (response.statusCode !== 200) {
-          console.log('ERROR: Response code is ' + response.statusCode);
-          process.exit(-1);
-        }
-
-        sendPem(body.id_token);
-      }
-    );
-  })
-  .fail(function(err) {
-    console.log(err);
-    process.exit(-1);
-  });
+  openBrowser(env);
+  getAuth0Token(env);
+});
